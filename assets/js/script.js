4107cd38b9a11281598bb5b852dbfc009b9f0fbc
Brendan Bryan <bbryan1294@gmail.com>
2025-08-14 20:34:59 -0500

    my_work gallery + shuffle.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-let navLinks = document.querySelectorAll('a.inner-link');
-console.log('These are the section links');
+// Navlinks + Navigation ============================================================================
+
+let navLinks = document.querySelectorAll("a.inner-link");
+console.log("These are the section links");
 console.log(navLinks);
 
 navLinks.forEach((item) => {
-    item.addEventListener('click', function(){
+    item.addEventListener("click", function () {
         // remove active class (highlight / blue circle) from previously selected section link button
         document.querySelector('nav ul li a.active').classList.remove('active')
         // add active class (highlight / blue circle) to the newly clicked section link button
@@ -14,24 +16,64 @@ navLinks.forEach((item) => {
         document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active')
         // the active style which allows the section to be displayed is defined in assets\scss\_structure.scss 
         console.log(`You clicked on ${item.getAttribute('href')}`);
-    })
+    });
 });
 
-document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function(){
-    document.querySelector('#sidebar').classList.toggle('open');
-});
+// Collapse Sidebar ============================================================================
+
+document
+    .querySelector("#sidebar .toggle-sidebar")
+    .addEventListener("click", function () {
+        document.querySelector("#sidebar").classList.toggle("open");
+    });
+
+// Typed.js =====================================================================================
 
 var options = {
-    strings: ['Front-End Web Developer', 'Web Designer', 'Back-End Web Developer'],
+    strings: [
+        "Front-End Web Developer",
+        "Web Designer",
+        "Back-End Web Developer",
+    ],
     loop: true,
     typeSpeed: 70,
-    backSpeed: 30
+    backSpeed: 30,
 };
 
-new Typed('.field h2', options);
+new Typed(".field h2", options);
 
-for(let i = 1; i <= 15; i++) {
-    let meteor = document.createElement('span');
-    meteor.classList = 'meteor';
-    document.querySelector('#home .meteor-shower').append(meteor);
+// Meteors =======================================================================================
+
+for (let i = 1; i <= 15; i++) {
+    let meteor = document.createElement("span");
+    meteor.classList = "meteor";
+    document.querySelector("#home .meteor-shower").append(meteor);
+}
+
+// My_Work Shuffle ===============================================================================
+
+const shuffleInstance = new Shuffle(
+    document.querySelector("#my_work .work-items"),
+    {
+        itemSelector: ".item",
+    }
+);
+
+const filterButtons = document.querySelectorAll("#my_work .filters button");
+
+filterButtons.forEach((item) => {
+    item.addEventListener("click", workFilter);
+});
+
+function workFilter() {
+    const clickedButton = event.currentTarget;
+    const clickedButtonGroups = clickedButton.getAttribute("data-group");
+    const activeButton = document.querySelector('#my_work .filters button.active');
+
+    activeButton.classList.remove('active');
+    clickedButton.classList.add('active');
+
+    console.log(event.currentTarget);
+
+    shuffleInstance.filter(clickedButtonGroups);
 }

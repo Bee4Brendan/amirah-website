1d43641b6706a958b33d10d5d1b32b1dc0c5c169
Brendan Bryan <bbryan1294@gmail.com>
2025-08-21 15:21:41 -0500

    Work Section dynamic modal content

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,34 +7,28 @@ console.log(navLinks);
 navLinks.forEach((item) => {
     item.addEventListener("click", function () {
         // remove active class (highlight / blue circle) from previously selected section link button
-        document.querySelector('nav ul li a.active').classList.remove('active')
+        document.querySelector("nav ul li a.active").classList.remove("active");
         // add active class (highlight / blue circle) to the newly clicked section link button
-        document.querySelector(`nav ul li a[href='${item.getAttribute('href')}']`).classList.add('active')
+        document.querySelector(`nav ul li a[href='${item.getAttribute("href")}']`).classList.add("active");
         // remove the active class from the previously selected section
-        document.querySelector('main > section.active').classList.remove('active')
+        document.querySelector("main > section.active").classList.remove("active");
         // add active class to the newly clicked section so it is displayed on the screen
-        document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active')
-        // the active style which allows the section to be displayed is defined in assets\scss\_structure.scss 
-        console.log(`You clicked on ${item.getAttribute('href')}`);
+        document.querySelector(`main > section${item.getAttribute("href")}`).classList.add("active");
+        // the active style which allows the section to be displayed is defined in assets\scss\_structure.scss
+        console.log(`You clicked on ${item.getAttribute("href")}`);
     });
 });
 
 // Collapse Sidebar ============================================================================
 
-document
-    .querySelector("#sidebar .toggle-sidebar")
-    .addEventListener("click", function () {
-        document.querySelector("#sidebar").classList.toggle("open");
-    });
+document.querySelector("#sidebar .toggle-sidebar").addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("open");
+});
 
 // Typed.js =====================================================================================
 
 var options = {
-    strings: [
-        "Front-End Web Developer",
-        "Web Designer",
-        "Back-End Web Developer",
-    ],
+    strings: ["Front-End Web Developer", "Web Designer", "Back-End Web Developer"],
     loop: true,
     typeSpeed: 70,
     backSpeed: 30,
@@ -52,12 +46,9 @@ for (let i = 1; i <= 15; i++) {
 
 // My_Work Shuffle + Modal =======================================================================
 
-const shuffleInstance = new Shuffle(
-    document.querySelector("#my_work .work-items"),
-    {
-        itemSelector: ".item",
-    }
-);
+const shuffleInstance = new Shuffle(document.querySelector("#my_work .work-items"), {
+    itemSelector: ".item",
+});
 
 const filterButtons = document.querySelectorAll("#my_work .filters button");
 
@@ -68,32 +59,41 @@ filterButtons.forEach((item) => {
 function workFilter() {
     const clickedButton = event.currentTarget;
     const clickedButtonGroups = clickedButton.getAttribute("data-group");
-    const activeButton = document.querySelector('#my_work .filters button.active');
+    const activeButton = document.querySelector("#my_work .filters button.active");
 
-    activeButton.classList.remove('active');
-    clickedButton.classList.add('active');
+    activeButton.classList.remove("active");
+    clickedButton.classList.add("active");
 
     console.log(event.currentTarget);
 
     shuffleInstance.filter(clickedButtonGroups);
 }
 
-var workModal = new bootstrap.Modal(document.getElementById('workModal'));
+var workModal = new bootstrap.Modal(document.getElementById("workModal"));
 const workElements = document.querySelectorAll("#my_work .work-items .wrap");
-workModal.show(); // TODO: Must be removed
+
 workElements.forEach((item) => {
-    item.addEventListener('click', function(){
+    item.addEventListener("click", function () {
+        document.querySelector("#workModal .modal-body img").setAttribute("src", item.getAttribute("data-image"));
+        document.querySelector("#workModal .modal-body .title").innerText = item.getAttribute("data-title");
+        document.querySelector("#workModal .modal-body .description").innerText = item.getAttribute("data-description");
+        document.querySelector("#workModal .modal-body .client .value").innerText = item.getAttribute("data-client");
+        document.querySelector("#workModal .modal-body .completed .value").innerText = item.getAttribute("data-completed");
+        document.querySelector("#workModal .modal-body .skills .value").innerText = item.getAttribute("data-skills");
+        document.querySelector("#workModal .modal-body .project-link a").setAttribute("href", item.getAttribute("data-project-link"));
+
         workModal.show();
-    })
+    });
 });
 
-var workModalElement = document.getElementById('workModal')
-workModalElement.addEventListener('show.bs.modal', function (event) {
-    document.getElementById('my_work').classList.add('blur');
+var workModalElement = document.getElementById("workModal");
+workModalElement.addEventListener("show.bs.modal", function (event) {
+    document.getElementById("my_work").classList.add("blur");
     document.getElementById("sidebar").classList.add("blur");
-})
+});
 
 workModalElement.addEventListener("hide.bs.modal", function (event) {
     document.getElementById("my_work").classList.remove("blur");
     document.getElementById("sidebar").classList.remove("blur");
+    document.activeElement.blur();
 });
